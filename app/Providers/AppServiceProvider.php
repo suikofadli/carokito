50dd3fc7911d0053f10663200aab40316a3e1f75
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2025-02-13 11:17:42 +0700

    another fix telescope
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists('Laravel\Telescope\TelescopeServiceProvider::class')) {
-            $this->app->register(Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(Laravel\Telescope\TelescopeServiceProvider::class);
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
         }
     }
 
