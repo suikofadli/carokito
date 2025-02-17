9124557fe5529b43c00372e77a313d67b0f1385d
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2025-02-17 15:15:39 +0700

    refactor default seo meta
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,41 +1,6 @@
-<script setup>
-import PopularPost from '@/Shared/PopularPost.vue';
-import { getAppName, getAppUrl, getSeoTitle } from '@/utils';
-import { Head, usePage } from '@inertiajs/vue3';
-import { computed } from 'vue';
-import Header from './Header.vue';
-
-const pageProps = usePage().props;
-
-const ads = computed(() => pageProps.advertisement)
-const popularPosts = computed(() => pageProps.popularPosts)
-
-const seoTitle = computed(() => getSeoTitle())
-const seoDescription = computed(() => "Berita terbaru, akurat dan terpercaya di Wartabengkulu.co. Baca di sini biar kamu gak ketinggalan informasi terbaru seputar Politik, Hukum, Ekonomi, Teknologi, dan Lainnya.")
-</script>
-
 <template>
 
-    <Head>
-        <meta property="description" :content="seoDescription" head-key="description" />
-
-        <meta property="og:type" content="website" head-key="og:type" />
-        <meta property="og:title" :content="seoTitle" head-key="og:title" />
-        <meta property="og:description" :content="seoDescription" head-key="og:description" />
-        <meta property="og:url" :content="getAppUrl()" head-key="og:url" />
-        <meta property="og:locale" content="id_ID" />
-        <meta property="og:site_name" :content="getAppName()" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" :content="seoTitle" head-key="twitter:title" />
-        <meta name="twitter:description" :content="seoDescription" head-key="twitter:description" />
-        <meta name="twitter:image" :content="getAppUrl() + '/logo.png'" head-key="twitter:image" />
-
-        <!-- Control how search engines crawl and index your pages. -->
-        <meta name="robots" content="index, follow">
-
-        <meta http-equiv="Content-Language" content="id">
-    </Head>
+    <DefaultSeoMeta />
 
     <Header />
 
@@ -94,3 +59,16 @@ const seoDescription = computed(() => "Berita terbaru, akurat dan terpercaya di 
         <p class="text-sm text-gray-500">© {{ new Date().getFullYear() }}. All Right Reserved.</p>
     </footer>
 </template>
+
+<script setup>
+import PopularPost from '@/Shared/PopularPost.vue';
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+import DefaultSeoMeta from './DefaultSeoMeta.vue';
+import Header from './Header.vue';
+
+const pageProps = usePage().props;
+
+const ads = computed(() => pageProps.advertisement)
+const popularPosts = computed(() => pageProps.popularPosts)
+</script>
---
+++ resources/js/Layouts/DefaultSeoMeta.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <Head>
+        <meta property="description" :content="seoDescription" head-key="description" />
+
+        <meta property="og:type" content="website" head-key="og:type" />
+        <meta property="og:title" :content="seoTitle" head-key="og:title" />
+        <meta property="og:description" :content="seoDescription" head-key="og:description" />
+        <meta property="og:url" :content="getAppUrl()" head-key="og:url" />
+        <meta property="og:locale" content="id_ID" />
+        <meta property="og:site_name" :content="getAppName()" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" :content="seoTitle" head-key="twitter:title" />
+        <meta name="twitter:description" :content="seoDescription" head-key="twitter:description" />
+        <meta name="twitter:image" :content="getAppUrl() + '/logo.png'" head-key="twitter:image" />
+
+        <!-- Control how search engines crawl and index your pages. -->
+        <meta name="robots" content="index, follow">
+
+        <meta http-equiv="Content-Language" content="id">
+    </Head>
+</template>
+
+<script setup>
+import { getAppName, getAppUrl, getSeoTitle } from '@/utils';
+import { Head } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const seoTitle = computed(() => getSeoTitle())
+const seoDescription = computed(() => "Berita terbaru, akurat dan terpercaya di Wartabengkulu.co. Baca di sini biar kamu gak ketinggalan informasi terbaru seputar Politik, Hukum, Ekonomi, Teknologi, dan Lainnya.")
+</script>
