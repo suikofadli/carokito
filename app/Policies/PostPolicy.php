6494b0b4c20675dbeda514c144b38922187d7b21
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2025-02-17 16:49:16 +0700

    fix: allow editor or admin edit post even it has published
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -10,7 +10,11 @@ class PostPolicy
 {
     public function edit(User $user, Post $post)
     {
-        return ! $post->is_published && (Gate::allows('manage-posts') || $post->user_id === $user->id);
+        if ($post->is_published) {
+            return Gate::allows('manage-posts');
+        }
+
+        return Gate::allows('manage-posts') || $post->user_id === $user->id;
     }
 
     public function delete(User $user, Post $post)
