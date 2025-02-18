0678dc2b7e732aa4b998e6fd3b550b067d70b23e
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2025-02-18 11:36:08 +0700

    test remove footer dot
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah@gmail.com>

--- resources/js/Shared/Copyright.vue
+++ resources/js/Shared/Copyright.vue
@@ -1,5 +1,5 @@
 <template>
     <p class="text-sm text-gray-500 p-3">
-        © {{ new Date().getFullYear() }}. All Right Reserved.
+        © {{ new Date().getFullYear() }}. All Right Reserved
     </p>
 </template>
